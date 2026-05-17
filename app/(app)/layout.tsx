7adc4cccb4ef89cbe8d23b595fb7413c6201fecb
Marino Brnardić <>
2026-05-17 23:39:20 +0200

    Comment out TopNav

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,11 +1,11 @@
-import { TopNav } from "@/components/nav/TopNav";
+// import { TopNav } from "@/components/nav/TopNav";
 import { BottomTabs } from "@/components/nav/BottomTabs";
 import { FlowGuardProvider } from "@/components/nav/FlowGuardContext";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <FlowGuardProvider>
-      <TopNav />
+      {/* <TopNav /> */}
       <main className="pt-24 pb-24 px-4 max-w-[640px] mx-auto min-h-dvh">
         {children}
       </main>
